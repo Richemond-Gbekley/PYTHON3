2cbcecbd681a80fc0944837de9c0b9bc4784898f
Richemond Gbekley <richemondgbekley@gmail.com>
2023-10-02 16:51:42 +0000

    Updated

--- Tutorial3.py
+++ Tutorial3.py
@@ -158,6 +158,171 @@ print(mydict.get("Age"))
 
 #Get Keys
 #The keys() method will return a list of all the keys in the dictionary.
-x = mydict.get()
+print("\n")
+print("Getting the list of all the return in the dictionary")
+print("*****************************************************")
+x = mydict.keys()
 print(x)
 
+#Get Values
+#The values() method will return a list of all the values in the dictionary.
+print("\n")
+print("Getting the values")
+print("*******************")
+y = mydict.values()
+print(y)
+#Adding a new key and a value in the dictionary
+
+
+print("\n")
+print("Adding a anew key and value ")
+print("****************************")
+mydict['Address'] = "A341/9"
+print(mydict)
+
+
+#Get Items
+#The items() method will return each item in a dictionary, as tuples in a list
+print("\n")
+print("Getting the list of items ")
+print("**************************")
+z=mydict.items()
+print(z)
+
+
+#Copy a Dictionary
+#You cannot copy a dictionary simply by typing dict2 = dict1, 
+# because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
+#There are ways to make a copy, one way is to use the built-in Dictionary method copy().
+
+
+print("\n")
+print("Copying dict ")
+print("**************")
+mydict1 = mydict.copy()
+print(mydict1)
+
+#if -else and key words in python3
+#Python Conditions and If statements
+#Python supports the usual logical conditions from mathematics:
+#Equals: a == b
+#Not Equals: a != b
+#Less than: a < b
+#Less than or equal to: a <= b
+#Greater than: a > b
+#Greater than or equal to: a >= b
+#These conditions can be used in several ways, most commonly in "if statements" and loops.
+
+#keywords
+#and
+#or
+#not
+
+
+#Using the if condition
+print("\n")
+print("Using the if condition ")
+print("************************")
+password ="1234"
+username ="Ken"
+
+user_password =input("Enter Password \n")
+user_username = input("Enter Username \n")
+
+if user_password == password  and user_username == username :
+  print("Successfully Logged in")  
+else :
+    print("Access denied , wrong credentials")
+    
+    #Elif
+#The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+print("\n")
+print("Using the Elif condition ")
+print("*************************")
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+  
+# The while loop
+#Python has two primitive loop commands:
+#while loops
+#for loops
+print("\n")
+print("While condition ")
+print("****************")
+count = 0
+while count <=5 :
+    print(count)
+    count += 1
+
+print("\n")
+print("While condition  with break")
+print("****************")
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+  
+print("\n")
+print("While condition  with continue")
+print("********************************")
+i = 0
+while i <= 6 :
+    i += 1
+    if i == 3 :
+        continue
+    print(i)
+    
+    print("\n")
+print("While condition  with else")
+print("***************************")
+count = 0
+while count <=5 :
+    print(count)
+    count += 1
+else:
+    print("i is exausted")
+    
+    
+#A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+#This is less like the for keyword in other programming languages,
+# and works more like an iterator method as found in other object-orientated programming languages.
+#With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+    print("\n")
+print("Foor Loop")
+print("***********")
+for x in mydict :
+    print(x)
+    
+    print("\n")
+print("For Loop with break")
+print("**********************")
+for x in mydict :
+    print(x)
+    if x == "Age" :
+        break
+        
+        
+   # print("\n")
+print("For Loop with break")
+print("**********************")
+for x in mydict :
+    
+    if x == "Age" :
+        break
+    print(x)
+    
+    print("For Loop with continue")
+print("****************************")
+for x in mydict :
+    
+    if x == "Age" :
+        continue
+    print(x)
+        
